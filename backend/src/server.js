3b1373fc2ec67028b99a1d0ea9044fcebb817cf0
Priyanshu Kumar <priyanshukrc000@gmail.com>
2026-08-24 00:44:03 +0530

    to be deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'                             //server
 import cors from "cors"
 import dotenv from 'dotenv'                               //.env
+import path from 'path'
+import fs from "fs"
 
 import notesRoutes from './routes/notesRoutes.js';        //api
 import { connectDB } from './config/db.js';               //db
@@ -9,15 +11,18 @@ import rateLimiter from './middleware/rateLimiter.js'
 
 dotenv.config()                                           //initialize .env 
 
-const PORT=process.env.PORT                               //accesing port from .env
 const app = express()                                     //calling express
+const PORT=process.env.PORT                               //accesing port from .env
+const __dirname = path.resolve()
                                                
 
-app.use(
+if(process.env.NODE_ENV !== "production"){
+    app.use(
     cors({
         origin:"http://localhost:5173",
     })
 )
+}
 app.use(express.json())                                   //middlewares - to access title & content from body (req.body) / parse JSON bodies
 app.use(rateLimiter);
 
@@ -27,6 +32,14 @@ app.use((req, res, next) => {                             //our simple custom mi
 })
 
 app.use('/api/notes', notesRoutes)                        // notes routes for CRUD Operations
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 connectDB().then( () => {                                   //calling fn from db.js for db connection ...
     app.listen(PORT, ()=>{                                    //listen port
