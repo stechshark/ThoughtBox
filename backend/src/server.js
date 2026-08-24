f3cf8ce232830aeea8979151f9cce522f99002c6
Priyanshu Kumar <priyanshukrc000@gmail.com>
2026-08-24 23:41:16 +0530

    final

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,49 +1,47 @@
-import express from 'express'                             //server
-import cors from "cors"
-import dotenv from 'dotenv'                               //.env
-import path from 'path'
-import fs from "fs"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './routes/notesRoutes.js';        //api
-import { connectDB } from './config/db.js';               //db
-import ratelimiter from './middleware/rateLimiter.js'
-import rateLimiter from './middleware/rateLimiter.js'
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-dotenv.config()                                           //initialize .env 
+dotenv.config();
 
-const app = express()                                     //calling express
-const PORT=process.env.PORT                               //accesing port from .env
-const __dirname = path.resolve()
-                                               
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production"){
-    app.use(
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
-        origin:"http://localhost:5173",
+      origin: "http://localhost:5173",
     })
-)
+  );
 }
-app.use(express.json())                                   //middlewares - to access title & content from body (req.body) / parse JSON bodies
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-app.use((req, res, next) => {                             //our simple custom middleware
-    console.log(`Req method is ${req.method} & Req URL is ${req.url}`)
-    next();
-})
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
-app.use('/api/notes', notesRoutes)                        // notes routes for CRUD Operations
+app.use("/api/notes", notesRoutes);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-connectDB().then( () => {                                   //calling fn from db.js for db connection ...
-    app.listen(PORT, ()=>{                                    //listen port
-    console.log(`Server up & running on PORT ${PORT}`)
-})
-})
-
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
