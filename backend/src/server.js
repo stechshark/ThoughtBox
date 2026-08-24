3404ff5525423bcc74c79d4a3c9328dcef3c0589
Priyanshu Kumar <priyanshukrc000@gmail.com>
2026-08-25 01:06:47 +0530

    this should work

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,10 +33,10 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
 
